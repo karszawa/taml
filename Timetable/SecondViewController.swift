4aa7a6cc4dfa96c3aee0f41777237b7adca399ab
siquare <cubicroonn@gmail.com>
2015-02-10 18:26:07 +0900

    Initial Commit

---
+++ Timetable/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Timetable
+//
+//  Created by Cubic on 2015/02/10.
+//  Copyright (c) 2015年 Cubic. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
